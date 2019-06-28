2c428f8fe609cacbd6b7cbde221069b9b41b437d
黑化肥发灰会挥发 <nppay_heifafei>
2019-06-28 14:50:54 +0800

    由 Cloud Studio 更新，https://studio.dev.tencent.com

---
+++ HelloWorld.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 文件名：HelloWorld.py
+
+#注释
+days = ['Monday', 'Tuesday', 'Wednesday',
+        'Thursday', 'Friday']
+
+if True:
+  print "你好，Python  "+days[0]
+else:
+  print "你好，世界"
+
+#等待用户输入
+raw_input("按下 enter 键退出，其他任意键显示...\n")
+
+
+'''
+多行注释
+多行注释
+多行注释
+'''
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CloudStudio
-腾讯云端工作空间
+[腾讯云端工作空间](https://studio.dev.tencent.com/dashboard/workspace)
+
+> 2019年6月28日14:25:31
+
+1.脚本文件添加可执行权限 ` $ chmod +x test.py   ` 
